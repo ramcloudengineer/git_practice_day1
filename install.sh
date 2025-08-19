d3cae127370b0fe39ff59855adbbbde34ac7f5ad
Ritendra Sahoo <ritendrasahoo343@gmail.com>
2025-08-19 21:25:58 +0530

    install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo "installing  $1"
+
+sudo apt-get update > /dev/null
+sudo apt-get install $1 -y
+
+echo "installation completed"
+
+
+
+
+
+tgdhfghdg fdufhjdh ufhdjhf dhfdh hfjdhjf 
